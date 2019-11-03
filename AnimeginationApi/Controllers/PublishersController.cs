d2d2eaa817edaa6ffa3162072d6a5a86b6c73747
Rey David <reydavid47@gmail.com>
2019-11-02 21:17:43 -0700

    Publishers Controller

--- AnimeginationApi/Controllers/PublishersController.cs
+++ AnimeginationApi/Controllers/PublishersController.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Web.Http;
 using AnimeginationApi.Models;
 using Swashbuckle.Swagger.Annotations;
+using AnimeginationApi.Filters;
+using System.Threading.Tasks;
 
 namespace AnimeginationApi.Controllers
 {
@@ -112,6 +114,92 @@ namespace AnimeginationApi.Controllers
 
         //    return Ok(publisher);
         //}
+
+        // PUT: api/Publishers
+        [HttpPut]
+        [AdminRoleFilter]
+        [JwtTokenFilter]
+        [SwaggerOperation("PutPublisher")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        [SwaggerResponse(HttpStatusCode.InternalServerError)]
+        public async Task<IHttpActionResult> PutPublisher([FromBody] Publisher publisherInput)
+        {
+            string userId = Request.UserId();
+
+            Publisher publisher = new Publisher {
+                PublisherID = publisherInput.PublisherID, 
+                PublisherName = publisherInput.PublisherName, 
+                Description = publisherInput.Description
+            };
+
+            db.Publishers.Add(publisher);
+            db.SaveChanges();
+
+            var response = new
+            {
+                PublisherID = publisher.PublisherID, 
+                PublisherName = publisher.PublisherName,
+                Description = publisher.Description
+            };
+            return Ok(response);
+        }
+
+        // POST: api/Publishers
+        [HttpPost]
+        [AdminRoleFilter]
+        [JwtTokenFilter]
+        [SwaggerOperation("PostPublisher")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        [SwaggerResponse(HttpStatusCode.InternalServerError)]
+        public async Task<IHttpActionResult> PostPublisher([FromBody] Publisher publisherInput)
+        {
+            if (!this.PublisherExists(publisherInput.PublisherID)) {
+                return NotFound();
+            }
+            Publisher publisher = db.Publishers.SingleOrDefault(item => 
+                item.PublisherID == publisherInput.PublisherID);
+            if (publisher == null) {
+                return NotFound();
+            }
+            publisher.PublisherName = publisherInput.PublisherName;
+            publisher.Description = publisherInput.Description;
+            db.SaveChanges();
+
+            var response = new {
+                PublisherID = publisher.PublisherID,
+                PublisherName = publisher.PublisherName, 
+                Description = publisher.Description
+            };
+            return Ok(response);
+        }
+
+        // DELETE: api/publishers/5
+        [HttpDelete]
+        [AdminRoleFilter]
+        [JwtTokenFilter]
+        [SwaggerOperation("DeletePublisher")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        public async Task<IHttpActionResult> DeletePublisher(int id)
+        {
+            // Delete Publishers Record
+            Publisher publisher = db.Publishers.SingleOrDefault(item => item.PublisherID == id);
+
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            db.Publishers.Remove(publisher);
+            //await db.SaveChangesAsync();
+            db.SaveChanges();
+
+            return Ok(publisher);
+        }
 
         protected override void Dispose(bool disposing)
         {
