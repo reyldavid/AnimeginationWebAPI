074db0312eeda4d38e8a4d52d204dedca6ebbc16
Rey David <reydavid47@gmail.com>
2019-11-05 18:22:56 -0800

    Products Controller

--- AnimeginationApi/Controllers/ProductsController.cs
+++ AnimeginationApi/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using AnimeginationApi.Models;
 using Swashbuckle.Swagger.Annotations;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using AnimeginationApi.Services;
 using AnimeginationApi.Filters;
@@ -39,16 +40,17 @@ namespace AnimeginationApi.Controllers
                 PublisherName = prod.Publisher.PublisherName,
                 OnSale = prod.OnSale,
                 RatingID = prod.RatingID
-            }).AsEnumerable();
+            }).OrderBy(pr => pr.ProductTitle)
+            .AsEnumerable();
 
             return products;
         }
 
         // GET: api/Products/5
+        // [JwtTokenFilter]
         [SwaggerOperation("GetProducts")]
         [SwaggerResponse(HttpStatusCode.OK)]
         [SwaggerResponse(HttpStatusCode.NotFound)]
-        //[JwtTokenFilter]
         public object GetProducts(int id)
         {
             //string username = Request.UserName();
@@ -182,6 +184,43 @@ namespace AnimeginationApi.Controllers
             return response;
         }
 
+        // [Route("api/products")]
+        [HttpPut]
+        [AdminRoleFilter]
+        [JwtTokenFilter]
+        [SwaggerOperation("PutProduct")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        [SwaggerResponse(HttpStatusCode.InternalServerError)]
+        public HttpResponseMessage PutProduct([FromBody] ApiProduct productInput)
+        {
+            Product product = db.Products.SingleOrDefault(pd => pd.ProductID == productInput.ProductID); 
+
+            if (product == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            product.ProductCode = string.IsNullOrEmpty(productInput.ProductCode) ? product.ProductCode : productInput.ProductCode;
+            product.ProductTitle = string.IsNullOrEmpty(productInput.ProductTitle) ? product.ProductTitle : productInput.ProductTitle;
+            product.ProductDescription = string.IsNullOrEmpty(productInput.ProductDescription) ? product.ProductDescription : productInput.ProductDescription;
+            product.CategoryID = productInput.CategoryID == 0 ? product.CategoryID : productInput.CategoryID;
+            product.MediumID = productInput.MediumID == 0 ? product.MediumID : productInput.MediumID;
+            product.OnSale =  productInput.OnSale;
+            product.ProductAgeRating = string.IsNullOrEmpty(productInput.ProductAgeRating) ? product.ProductAgeRating : productInput.ProductAgeRating;
+            product.ProductImageURL = string.IsNullOrEmpty(productInput.ProductImageURL) ? product.ProductImageURL : productInput.ProductImageURL;
+            product.ProductLength = productInput.ProductLength == 0 ? product.ProductLength : productInput.ProductLength;
+            product.ProductYearCreated = productInput.ProductYearCreated == 0 ? product.ProductYearCreated : productInput.ProductYearCreated;
+            product.PublisherID = productInput.PublisherID == 0 ? product.PublisherID : productInput.PublisherID;
+            product.RatingID = productInput.RatingID == 0 ? product.RatingID : productInput.RatingID;
+            product.UnitPrice = productInput.UnitPrice == 0 ? product.UnitPrice : productInput.UnitPrice;
+            product.YourPrice = productInput.YourPrice == 0 ? product.YourPrice : productInput.YourPrice;
+
+            db.SaveChanges();
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
         //// DELETE: api/Products/5
         //[ResponseType(typeof(Product))]
         //public async Task<IHttpActionResult> DeleteProduct(int id)
@@ -197,6 +236,32 @@ namespace AnimeginationApi.Controllers
 
         //    return Ok(product);
         //}
+
+        // DELETE: api/products/5
+        [HttpDelete]
+        [AdminRoleFilter]
+        [JwtTokenFilter]
+        [SwaggerOperation("DeleteProduct")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        public async Task<IHttpActionResult> DeleteProduct(int id)
+        {
+            // Delete Product Record
+            Product product = db.Products.SingleOrDefault(item => item.ProductID == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Products.Remove(product);
+            //await db.SaveChangesAsync();
+            db.SaveChanges();
+
+            return Ok(product);
+        }
 
         protected override void Dispose(bool disposing)
         {
